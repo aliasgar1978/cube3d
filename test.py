0124b7b8530dc9f984c7679a8067bb154b18c565
aliasgar1978 <aholo2000@gmail.com>
2021-07-17 09:27:39 +0530

    remark update

--- test.py
+++ test.py
@@ -15,8 +15,8 @@ front, back, top, bottom, left, right = ('front', 'back', 'top', 'bottom', 'left
 CB = get_cube("cube.xlsx")
 
 # ----------------------------------------------------------------------------- #
-# Otherway to create a cube statically, 
-# instead of providing Excel file as input
+# Another way to create a cube with static points: 
+# (instead of providing Excel file as input)
 # ----------------------------------------------------------------------------- #
 # 1. Create 27 x "Point" objects
 #    ex: P1 = Point(x=-1 ,y=-1  ,z=-1 , x_color='yellow', y_color='blue', z_color='red')
@@ -30,7 +30,7 @@ CB = get_cube("cube.xlsx")
 
 
 # ----------------------------------------------------------------------------- #
-# Rotate the square face/view to a direction
+# use below rotate_square method to Rotate a square/face/view to a direction
 # view = defines the face which require to be rotated
 # clockwise = direction looking from front of the view 
 #             ( True=Clockwise direction, False=Anticlockwise direction ), 
@@ -40,14 +40,14 @@ CB.rotate_square(view=front, clockwise=False)
 
 
 # ----------------------------------------------------------------------------- #
-# Change the Cube view to another direction
+# use below change_to method to Change the Cube view to another direction
 # view = defines the face to which we want to face.
 # ----------------------------------------------------------------------------- #
 CB = CB.change_to(view=left)
 
 
 # ----------------------------------------------------------------------------- #
-# Displaying various parameters
+# Verification/Display - parameters
 # ----------------------------------------------------------------------------- #
 
 # ### print if the cube is solved or not ###
